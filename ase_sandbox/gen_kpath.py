7edbd808c2f6994b49c03ea1fb57918952a3a15c
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-06-20 09:38:42 +0700

    Removing unneeded codes in gen_kpath

--- ase_sandbox/gen_kpath.py
+++ ase_sandbox/gen_kpath.py
@@ -3,45 +3,16 @@ from ase.units import Bohr
 import sys
 
 def gen_kpath( atoms, lattice, Nkpts=60 ):
+    """
+    Automatically generate k-points for a band structure calculation.
+    """
     #
     points = special_points[lattice]
     paths = parse_path_string(special_paths[lattice])
     print(paths[0])
     kpts_spec = [points[k] for k in paths[0]]
     kpts, x, Xkpt = get_bandpath(kpts_spec,atoms.cell)
+    #
+    # TODO: also return string for special k-points' symbol
+    # probably using variable `paths`.
     return kpts, x, Xkpt
-
-    """
-    #
-    if lattice=='fcc':
-        points = ibz_points[lattice]
-        G = points['Gamma']
-        X = points['X']
-        W = points['W']
-        K = points['K']
-        L = points['L']
-        kpts, x, Xkpt = get_bandpath([W, L, G, X, W, K], atoms.cell, npoints=Nkpts)
-    #
-    elif lattice=='hexagonal':
-        points = ibz_points[lattice]
-        G = points['Gamma']
-        M = points['M']
-        K = points['K']
-        A = points['A']
-        L = points['L']
-        kpts, x, Xkpt = get_bandpath([G, M, K, G, A, L], atoms.cell, npoints=Nkpts)
-    #
-    elif lattice=='tetragonal':
-        points = ibz_points[lattice]
-        G = points['G']
-        M = points['M']
-        Z = points['Z']
-        X = points['X']
-        A = points['A']
-        R = points['R']
-        kpts, x, Xkpt = get_bandpath([G, Z, A, M, G, X, R], atoms.cell, npoints=NKPT)
-    else:
-        raise RuntimeError('Unknown lattice: %s' % lattice)
-    #
-    return kpts, x, Xkpt
-    """
