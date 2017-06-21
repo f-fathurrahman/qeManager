48792aac96b593c84ad5a1798fcda1817707df79
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-06-21 13:31:50 +0700

    Test elemental bulk

--- ase_sandbox/bulk/elemental_bulk.py
+++ ase_sandbox/bulk/elemental_bulk.py
@@ -1,7 +1,9 @@
+#!/usr/bin/python
 from __future__ import print_function
-
+import sys
 from ase.build import bulk
 # ase.build.bulk(name, crystalstructure=None, a=None, c=None, covera=None, u=None, orthorhombic=False, cubic=False)
 
-atoms = bulk('Cu')
-atoms.write('Cu.xsf')
+atomName = sys.argv[1]
+atoms = bulk(atomName)
+atoms.write(atomName + '.xsf')
