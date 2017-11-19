667f706b3cb1ec0f6b53c4c81be5617e1fa38f05
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-11-19 23:07:13 +0700

    Add NWChemOutput

---
+++ nwchem/NWChemOutput.py
@@ -0,0 +1,16 @@
+from __future__ import print_function
+
+class NWChemOutput:
+
+    def __init__(self,outfile):
+        self.outfile = outfile
+        f = open(outfile,'r')
+
+        f.close()
+
+    
+    def get_mo(self):
+        pass
+
+
+
