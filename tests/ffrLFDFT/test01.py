f00d5fa780b30197962cb17e0362000aee142a89
Fadjar Fathurrahman <fadjar.fathurrahman@gmail.com>
2017-12-14 13:41:53 +0700

    Update test, run ffr_LFDFT calc

--- tests/ffrLFDFT/test01.py
+++ tests/ffrLFDFT/test01.py
@@ -8,7 +8,10 @@ import numpy as np
 import sys
 sys.path.append('/home/efefer/WORKS/my_github_repos/')
 
-from qeManager.ffr_lfdft import *
+from qeManager.ffr_lfdft.ControlNameList import *
+from qeManager.ffr_lfdft.SystemNameList import *
+from qeManager.ffr_lfdft.ElectronsNameList import *
+from qeManager.pwscf.utils_write import write_atomic_positions, write_atomic_species
 
 from ase.build import molecule
 
@@ -25,7 +28,7 @@ atoms.center()
 # several simple functions.
 
 ctrl_NL = ControlNameList()
-ctrl_NL.pseudo_dir = '../../pseudopotentials/pade_gth'
+ctrl_NL.pseudo_dir = './'  # this is the default, anyway
 ctrl_NL.write_all()
 
 sys_NL = SystemNameList(atoms)
@@ -34,9 +37,7 @@ sys_NL.write_all()
 elec_NL = ElectronsNameList()
 elec_NL.write_all()
 
-"""
-pspFiles = ['H.q1.gth', 'N.q5.gth']
+pspFiles = ['H-q1.gth', 'N-q5.gth']
+# write to stdout
 write_atomic_species( atoms, pspFiles=pspFiles )
-write_atomic_positions(atoms)
-write_cell(atoms)
-"""
+write_atomic_positions( atoms )
